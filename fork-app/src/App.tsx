3306b81f5838ea8a708288fe0577fe8008553220
collinzhu <345607645@qq.com>
2020-08-20 14:59:20 +1200

    basic component

--- fork-app/src/App.tsx
+++ fork-app/src/App.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React,{useState} from 'react';
+import {IUserInput} from './Common/Interface';
+import SearchBar from './Component/SearchComponent/SearchBar';
+import TableDisplay from './Component/TableComponent/TableDisplay'
 
 function App() {
+  const [UserInput, setUserInput] = useState<IUserInput>({
+    SearchQuery: ""
+  });
+  function SetUserInput(a: IUserInput){
+    setUserInput(a)
+  }
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <SearchBar SetUserInput = {(a: IUserInput) => SetUserInput(a)}/>
+      <TableDisplay></TableDisplay>
     </div>
   );
 }
---
+++ fork-app/src/Component/TableComponent/TableDisplay.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+function TableDisplay(){
+    return(
+        <div>
+            <Table className="TableClass">
+                <TableHead>
+                <TableRow>
+                    <TableCell>Dessert (100g serving)</TableCell>
+                    <TableCell align="right">Calories</TableCell>
+                    <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                    <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                    <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                </TableRow>
+                </TableHead>
+                <TableBody>
+                </TableBody>
+            </Table>
+            
+        </div>
+    )
+}
+
+
+export default TableDisplay
